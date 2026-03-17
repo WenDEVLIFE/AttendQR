46e5199ca77119041d922e352e2882a3f297fb35
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-03-17 13:01:25 +0800

    Initial Commit

---
+++ AttendQR/AttendQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendQRApp.swift
+//  AttendQR
+//
+//  Created by Frouen on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct AttendQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
